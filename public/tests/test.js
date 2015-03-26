511bff7a4a51fc25e73341ef293efdc3c46aeeed
alu0100315462 <alu0100315462@ull.edu.es>
2015-03-26 23:54:09 +0000

    mas tests

--- public/tests/test.js
+++ public/tests/test.js
@@ -24,5 +24,23 @@ suite('Analizador Lexico de un subconjunto', function() {
     assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "b",\n        "arity": "name"\n    },\n    "second": {\n        "value": "function",\n        "arity": "function",\n        "first": [\n            {\n                "value": "x",\n                "arity": "name"\n            }\n        ],\n        "second": {\n            "value": "return",\n            "arity": "statement",\n            "first": {\n                "value": 1,\n                "arity": "literal"\n            }\n        }\n    }\n}');
   });
   
+  test('Probando variables numericas.', function() {
+    INPUT.value = 'var num = 5 * 2;';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "num",\n        "arity": "name"\n    },\n    "second": {\n        "value": "*",\n        "arity": "binary",\n        "first": {\n            "value": 5,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 2,\n            "arity": "literal"\n        }\n    }\n}');
+  });
+  
+  test('Probando un while.', function() {
+    INPUT.value = 'while (1 !== 0) {}';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "while",\n    "arity": "statement",\n    "first": {\n        "value": "!==",\n        "arity": "binary",\n        "first": {\n            "value": 1,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 0,\n            "arity": "literal"\n        }\n    },\n    "second": null\n}');
+  });
+  
+    test('Probando if else.', function() {
+    INPUT.value = 'if (0 < 1) {}\nelse {}';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "if",\n    "arity": "statement",\n    "first": {\n        "value": "<",\n        "arity": "binary",\n        "first": {\n            "value": 0,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 1,\n            "arity": "literal"\n        }\n    },\n    "second": null,\n    "third": null\n}');
+  });
   
 });
+
