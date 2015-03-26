ff51058109de19ef1a60e6298ddb9c96fc3b5353
ctc87 <carlos.troyano.carmona@gmail.com>
2015-03-26 14:41:05 +0000

    reformando rama heroku

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var expressLayouts = require('express-ejs-layouts');
 var csv = require('./server.js');
 
 // ¿Estos modulos los necesitamos?
-var _ = require('underscore');
-var $ = require('jquery');
+//var _ = require('underscore');
+//var $ = require('jquery');
 
 
 // Definimos la configuración de la aplicación.
@@ -15,7 +15,8 @@ app.set('port', (process.env.PORT || 3000));  // Puerto de escucha
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.use(express.static(__dirname + '/'));
+//app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
 
 //  Asociamos cada ruta con una función encargada de controlar la acción a ejecutar.
@@ -24,13 +25,18 @@ app.get('/', function (request, response) {
 });
 
 // ¿Esta funcion la necesitamos?
-app.get('/ajaxEx/:cad', function (request, response) {
-   response.send(JSON.stringify(csv.calculate(request.params.cad)));
-});
+//app.get('/ajaxEx/:cad', function (request, response) {
+//   response.send(JSON.stringify(csv.calculate(request.params.cad)));
+//});
 
 // ¿Esta función la necesitamos?
 app.get('/tests/', function (request, response) {
    response.render('/tests/index', { title: 'CSV test' });
+
+app.get('/pr', function (request, response) {
+	    response.send({ "rows": rows });
+}
+
 
 // Arrancamos el servidor
 app.listen(app.get('port'), function () {
