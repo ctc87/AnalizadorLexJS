e1a8e2d41d17339509a3d48dc8a9f85dbe9f87b9
ctc87 <carlos.troyano.carmona@gmail.com>
2015-03-27 12:51:50 +0000

    f

--- public/tests/test.js
+++ public/tests/test.js
@@ -2,18 +2,9 @@ var assert = chai.assert;
 
 	
 suite('Analizador Lexico de un subconjunto', function() {
-
-  setup(function(){
-    //if (typeof __html__ !== 'undefined') {
-      document.body.innerHTML = __html__['tests/index.html'];
-      input = document.getElementById('IMP');
-      output = document.getElementById('OUTPUT');
-    //}
-  });
-
   
   test('Declaración de variable.', function() {
-    input.value = 'var hola = "holaMundo";';
+    INP.value = 'var hola = "holaMundo";';
     main();
     assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "hola",\n        "arity": "name"\n    },\n    "second": {\n        "value": "holaMundo",\n        "arity": "literal"\n    }\n}');
   });
