27f35d534447dc0b92a768f8fd00e76e88f51cbe
alu0100315462 <alu0100315462@ull.edu.es>
2015-03-27 01:14:50 +0000

    tests agregados a rama heroku #3

--- public/tests/test.js
+++ public/tests/test.js
@@ -6,9 +6,9 @@ suite('Analizador Lexico de un subconjunto', function() {
 
   setup(function(){
     if (typeof __html__ !== 'undefined') {
-      document.body.innerHTML = __html__['tests/index.html'];
+      document.body.innerHTML = __html__['public/tests/index.html'];
       input = document.getElementById('INPUT');
-      ouput = document.getElementById('OUTPUT');
+      output = document.getElementById('OUTPUT');
     }
   });
   
@@ -24,5 +24,22 @@ suite('Analizador Lexico de un subconjunto', function() {
     assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "b",\n        "arity": "name"\n    },\n    "second": {\n        "value": "function",\n        "arity": "function",\n        "first": [\n            {\n                "value": "x",\n                "arity": "name"\n            }\n        ],\n        "second": {\n            "value": "return",\n            "arity": "statement",\n            "first": {\n                "value": 1,\n                "arity": "literal"\n            }\n        }\n    }\n}');
   });
   
+  test('Probando variables numericas.', function() {
+    INPUT.value = 'var num = 5 * 2;';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "num",\n        "arity": "name"\n    },\n    "second": {\n        "value": "*",\n        "arity": "binary",\n        "first": {\n            "value": 5,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 2,\n            "arity": "literal"\n        }\n    }\n}');
+  });
+  
+  test('Probando un while.', function() {
+    INPUT.value = 'while (1 !== 0) {}';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "while",\n    "arity": "statement",\n    "first": {\n        "value": "!==",\n        "arity": "binary",\n        "first": {\n            "value": 1,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 0,\n            "arity": "literal"\n        }\n    },\n    "second": null\n}');
+  });
+  
+    test('Probando if else.', function() {
+    INPUT.value = 'if (0 <= 1) {}';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "if",\n    "arity": "statement",\n    "first": {\n        "value": "&lt;=",\n        "arity": "binary",\n        "first": {\n            "value": 0,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 1,\n            "arity": "literal"\n        }\n    },\n    "second": null,\n    "third": null\n}');
+  });
   
 });
